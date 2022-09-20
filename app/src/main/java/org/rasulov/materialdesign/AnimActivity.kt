08c13457260a791142235ff1de97bdbabf91fe27
ITrf0088 <itrf0088@gmail.com>
2022-09-20 15:28:15 +0500

    Heart(Like) animation has been made

--- app/src/main/java/org/rasulov/materialdesign/AnimActivity.kt
+++ app/src/main/java/org/rasulov/materialdesign/AnimActivity.kt
@@ -5,11 +5,13 @@ import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.addListener
+import androidx.core.app.ActivityCompat
 import org.rasulov.materialdesign.databinding.ActivityAnimBinding
 
 class AnimActivity : AppCompatActivity() {
@@ -20,9 +22,19 @@ class AnimActivity : AppCompatActivity() {
         val binding = ActivityAnimBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
 
-        binding.btnStart.setOnClickListener { _ ->
+        var isTrue = true
+
+        binding.checkmark.setOnClickListener { _ ->
+
+            binding.checkmark.background = if (isTrue) {
+                ActivityCompat.getDrawable(this, R.drawable.avd_heart_outline)
+            } else {
+                ActivityCompat.getDrawable(this, R.drawable.avd_heart_fill)
+            }
+
             val animation = binding.checkmark.background as AnimatedVectorDrawable
             animation.start()
+            isTrue = !isTrue
         }
 
     }
