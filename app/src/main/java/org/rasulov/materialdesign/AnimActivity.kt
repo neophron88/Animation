7927794a7fd26a224a6b022c7fc035ec63c29db3
ITrf0088 <itrf0088@gmail.com>
2022-09-20 12:59:24 +0500

    CheckMark animation has been made

--- app/src/main/java/org/rasulov/materialdesign/AnimActivity.kt
+++ app/src/main/java/org/rasulov/materialdesign/AnimActivity.kt
@@ -4,6 +4,7 @@ import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.graphics.Color
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -18,39 +19,10 @@ class AnimActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityAnimBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        var counter = 0
 
-        binding.apply {
-            btnStart.setOnClickListener { _ ->
-                counter += 1
-
-                Log.d("it0088", "onCreate: ${btnStart.translationY}")
-                Log.d("it0088", "onCreate: ${btnStart.y}")
-                Log.d("it0088", "onCreate: ${btnStart.scaleX}")
-
-                val translate = ObjectAnimator.ofFloat(
-                    btnStart, "translationY", (100f * counter)
-                )
-
-                val scale = ObjectAnimator.ofFloat(
-                    btnStart, "scaleX", (1f * counter)
-                )
-
-                val color = ObjectAnimator.ofObject(
-                    btnStart, "textColor", ArgbEvaluator(), Color.RED, Color.GREEN
-                )
-
-                AnimatorSet().apply {
-                    playTogether(translate, scale, color)
-                    duration = 1000
-                    addListener(onEnd = {
-                        Log.d("it0088", "onCreate: ${btnStart.translationY}")
-                        Log.d("it0088", "onCreate: ${btnStart.y}")
-                        Log.d("it0088", "onCreate: ${btnStart.scaleX}")
-                    })
-                }.start()
-
-            }
+        binding.btnStart.setOnClickListener { _ ->
+            val animation = binding.checkmark.background as AnimatedVectorDrawable
+            animation.start()
         }
 
     }
